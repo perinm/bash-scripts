3af8b5c15b0ee53d76831a194035e657c5a1ba57
Lucas Manchine <lucasperinm@gmail.com>
2021-07-28 11:51:52 -0300

    added vlc

--- new_system.sh
+++ new_system.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install gdebi -y
+sudo apt install gdebi vlc -y
 COMMAND=google-chrome
 if ! command -v $COMMAND &> /dev/null; then
     wget -O ~/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
