168bac55b1d24f498cbaa7e470cd0a2fda623ccf
Lucas Manchine <lucasperinm@gmail.com>
2021-09-01 15:08:38 -0300

    minor vpn configs

--- extra_things.sh
+++ extra_things.sh
@@ -13,7 +13,10 @@ COMMAND=nordvpn
 if ! command -v $COMMAND &> /dev/null; then
     sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
     # nordvpn set obfuscate on
+    nordvpn set firewall on
     nordvpn set killswitch on
+    nordvpn set autoconnect on
+    nordvpn set ipv6 on
     nordvpn set dns 1.1.1.1 8.8.8.8 8.8.4.4
     # nordvpn set autoconnect on Obfuscated_Servers
 else
