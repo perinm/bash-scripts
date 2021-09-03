842ddbc38aab41c2b74feb353a1d3ae617f5fdfe
Lucas Manchine <lucasperinm@gmail.com>
2021-09-03 11:20:37 -0300

    testing things out

--- extra_things.sh
+++ extra_things.sh
@@ -1,15 +1,4 @@
-# install nordvpn
 COMMAND=nordvpn
-# if ! command -v $COMMAND &> /dev/null; then
-#     wget -O ~/nordvpn.deb https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-#     sudo gdebi ~/nordvpn.deb
-#     sudo apt update
-#     sudo apt install -y nordvpn
-#     # nordvpn login
-#     # nordvpn connect
-# else
-#     echo "$COMMAND found"
-# fi
 if ! command -v $COMMAND &> /dev/null; then
     sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
     # nordvpn set obfuscate on
