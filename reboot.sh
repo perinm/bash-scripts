62f19f18ae82e92e2bd99d9398e83b511a2dfae3
Lucas Manchine <lucasperinm@gmail.com>
2021-09-16 10:32:56 -0300

    fix #2

---
+++ reboot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# source: https://stackoverflow.com/questions/10408816/how-do-i-use-the-nohup-command-without-getting-nohup-out
+nohup discord --start-minimized >/dev/null 2>&1 &
+nohup slack -u >/dev/null 2>&1 &
+nohup telegram-desktop -startintray >/dev/null 2>&1 &
+nohup upwork >/dev/null 2>&1 &
