926f67a4c0b2062e7f3496f74379b1a6d1570e58
Lucas Manchine <lucasperinm@gmail.com>
2022-01-10 14:17:02 -0300

    added brave-browser

--- new_system.sh
+++ new_system.sh
@@ -196,6 +196,16 @@ if ! command -v $COMMAND &> /dev/null; then
 else
     echo "$COMMAND found"
 fi
+COMMAND=brave-browser
+if ! command -v $COMMAND &> /dev/null; then
+    sudo apt install apt-transport-https curl
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update
+    sudo apt install -y brave-browser
+else
+    echo "$COMMAND found"
+fi
 FILE=~/.local/share/applications/freecad_realthunder.desktop
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
