ad66ea04c6f4e816b380b77a25c3fd45850f1a37
Lucas Manchine <lucasperinm@gmail.com>
2022-04-21 13:22:17 -0300

    starting modifications for ubuntu 22.04 lts

--- new_system.sh
+++ new_system.sh
@@ -30,9 +30,11 @@ else
 fi
 COMMAND=code
 if ! command -v $COMMAND &> /dev/null; then
-    sudo apt install -y software-properties-common apt-transport-https curl
-    curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+    sudo apt-get install -y wget gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
     sudo apt update
     sudo apt install -y code
     declare -a StringArray=(
