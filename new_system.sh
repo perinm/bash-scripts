f0c289a53bad1eca543e18d6c257c0bf0b18a6ac
Lucas Manchine <lucasperinm@gmail.com>
2022-04-22 11:53:00 -0300

    updated new_system.sh to ubuntu 22.04 succesfully

--- new_system.sh
+++ new_system.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 
+# TO-DO:
+# - Arduino
+# - Prusa
+# - Drovio
+# - Fritzing
+
 # lines below sudo apt install, install docker requirements
 sudo apt install -y gdebi python3-pip python3-venv htop libcanberra-gtk-module p7zip-full lm-sensors wireshark ppa-purge wireguard wireguard-tools net-tools nmap \
     apt-transport-https \
@@ -80,13 +86,13 @@ if ! command -v $COMMAND &> /dev/null; then
 else
     echo "$COMMAND found"
 fi
-COMMAND=drovio
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/drovio.deb https://repository.drovio.com/stable/drovio/linux/latest_version/drovio.deb
-    sudo gdebi -n ~/drovio.deb
-else
-    echo "$COMMAND found"
-fi
+# COMMAND=drovio
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/drovio.deb https://repository.drovio.com/stable/drovio/linux/latest_version/drovio.deb
+#     sudo gdebi -n ~/drovio.deb
+# else
+#     echo "$COMMAND found"
+# fi
 COMMAND=spotify
 if ! command -v $COMMAND &> /dev/null; then
     # curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo gpg --dearmor -o /usr/share/keyrings/spotify-archive-keyring.gpg
@@ -98,9 +104,9 @@ else
 fi
 COMMAND=obs
 if ! command -v $COMMAND &> /dev/null; then
-    sudo apt install -y ffmpeg
-    sudo add-apt-repository ppa:obsproject/obs-studio -y
-    sudo apt update
+    # sudo apt install -y ffmpeg
+    # sudo add-apt-repository ppa:obsproject/obs-studio -y
+    # sudo apt update
     sudo apt install -y obs-studio
 else
     echo "$COMMAND found"
@@ -122,23 +128,25 @@ else
 fi
 COMMAND=slack
 if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/slack.deb https://downloads.slack-edge.com/releases/linux/4.17.0/prod/x64/slack-desktop-4.17.0-amd64.deb
+    wget -O ~/slack.deb https://downloads.slack-edge.com/releases/linux/4.25.0/prod/x64/slack-desktop-4.25.0-amd64.deb
     sudo gdebi -n ~/slack.deb
+    # sudo snap install slack --classic
 else
     echo "$COMMAND found"
 fi
-COMMAND=upwork
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/upwork.deb https://upwork-usw2-desktopapp.upwork.com/binaries/v5_5_0_11_61df9c99b6df4e7b/upwork_5.5.0.11_amd64.deb
-    sudo gdebi -n ~/upwork.deb
-else
-    echo "$COMMAND found"
-fi
+# COMMAND=upwork
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/upwork.deb https://upwork-usw2-desktopapp.upwork.com/binaries/v5_6_10_1_de501d28cc034306/upwork_5.6.10.1_amd64.deb
+#     sudo gdebi -n ~/upwork.deb
+# else
+#     echo "$COMMAND found"
+# fi
 COMMAND=smplayer
 if ! command -v $COMMAND &> /dev/null; then
-    sudo add-apt-repository ppa:rvm/smplayer -y
-    sudo apt-get update 
-    sudo apt-get install -y smplayer smplayer-themes
+    # sudo add-apt-repository ppa:rvm/smplayer -y
+    # sudo apt-get update 
+    # sudo apt-get install -y smplayer smplayer-themes smplayer-skins 
+    sudo snap install smplayer
 else
     echo "$COMMAND found"
 fi
@@ -148,49 +156,59 @@ if ! command -v $COMMAND &> /dev/null; then
 else
     echo "$COMMAND found"
 fi
-FILE=~/.local/share/applications/obsidian.desktop
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-else
-    pip install 'lxml == 4.6.3'
-    python3 ./python_scripts/download_latest_file_from_github.py
-    sudo chmod a+x ${HOME}/apps/obsidian/Obsidian.AppImage
-    cat >$FILE <<EOL
-[Desktop Entry]
-Name=Obsidian
-Comment=Obsidian - A second brain, for you, forever.
-Exec=${HOME}/apps/obsidian/Obsidian.AppImage
-Icon=${HOME}/apps/app-icons/obsidian.png
-Terminal=false
-Type=Application
-Categories=Development
-MimeType=x-scheme-handler/obsidian;text/html;
-EOL
-fi
-COMMAND=arduino
+COMMAND=obsidian
 if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/arduino1.tar.xz https://downloads.arduino.cc/arduino-1.8.15-linux64.tar.xz
-    tar -xf ~/arduino1.tar.xz -C ~/
-    sudo ~/arduino-1.8.15/install.sh
-    sudo usermod -a -G dialout $USER
+    wget -O ~/obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v0.14.6/obsidian_0.14.6_amd64.deb
+    sudo gdebi -n ~/obsidian.deb
 else
     echo "$COMMAND found"
 fi
-COMMAND=qbittorrent
-if ! command -v $COMMAND &> /dev/null; then
-    sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
-    sudo apt-get update && sudo apt-get install -y qbittorrent
-else
-    echo "$COMMAND found"
-fi
-COMMAND=remmina
-if ! command -v $COMMAND &> /dev/null; then
-    sudo apt-add-repository ppa:remmina-ppa-team/remmina-next -y
-    sudo apt update
-    sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-secret
-else
-    echo "$COMMAND found"
-fi
+# COMMAND=arduino
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/arduino1.tar.xz https://downloads.arduino.cc/arduino-1.8.15-linux64.tar.xz
+#     tar -xf ~/arduino1.tar.xz -C ~/
+#     sudo ~/arduino-1.8.15/install.sh
+#     sudo usermod -a -G dialout $USER
+# else
+#     echo "$COMMAND found"
+# fi
+# FILE=~/.local/share/applications/arduinoide-beta.desktop
+# if [ -f "$FILE" ]; then
+#     echo "$FILE exists."
+# else
+#     pip3 install 'lxml == 4.6.3'
+#     python3 ./python_scripts/download_latest_arduino_from_github.py
+#     sudo chmod a+x ${HOME}/apps/freecad_realthunder/ArduinoIDE_BETA.AppImage
+#     cat >$FILE <<EOL
+# [Desktop Entry]
+# Type=Application
+# Name=Arduino IDE BETA
+# GenericName=Arduino IDE BETA
+# Comment=Open-source electronics prototyping platform
+# Exec=${HOME}/apps/arduinoide_beta/ArduinoIDE_BETA.AppImage
+# Icon=${HOME}/apps/app-icons/ArduinoIDE_BETA.png
+# Terminal=false
+# Categories=Development;IDE;Electronics;
+# MimeType=text/x-arduino-beta;
+# Keywords=embedded electronics;electronics;avr;microcontroller;
+# StartupWMClass=processing-app-Base
+# EOL
+# fi
+# COMMAND=qbittorrent
+# if ! command -v $COMMAND &> /dev/null; then
+#     sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
+#     sudo apt-get update && sudo apt-get install -y qbittorrent
+# else
+#     echo "$COMMAND found"
+# fi
+# COMMAND=remmina
+# if ! command -v $COMMAND &> /dev/null; then
+#     sudo apt-add-repository ppa:remmina-ppa-team/remmina-next -y
+#     sudo apt update
+#     sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-secret
+# else
+#     echo "$COMMAND found"
+# fi
 # un-comment to re-add teams
 # COMMAND=teams
 # if ! command -v $COMMAND &> /dev/null; then
@@ -201,34 +219,34 @@ fi
 # else
 #     echo "$COMMAND found"
 # fi
-COMMAND=brave-browser
-if ! command -v $COMMAND &> /dev/null; then
-    sudo apt install apt-transport-https curl
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt update
-    sudo apt install -y brave-browser
-else
-    echo "$COMMAND found"
-fi
-FILE=~/.local/share/applications/freecad_realthunder.desktop
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-else
-    pip3 install 'lxml == 4.6.3'
-    python3 ./python_scripts/download_latest_freecad_from_github.py
-    sudo chmod a+x ${HOME}/apps/freecad_realthunder/FreeCad_RealThunder.AppImage
-    cat >$FILE <<EOL
-[Desktop Entry]
-Name=FreeCad_RealThunder
-Comment=FreeCad RealThunder version
-Exec=${HOME}/apps/freecad_realthunder/FreeCad_RealThunder.AppImage
-Icon=${HOME}/apps/app-icons/freecad_realthunder.png
-Terminal=false
-Type=Application
-Categories=Development
-EOL
-fi
+# COMMAND=brave-browser
+# if ! command -v $COMMAND &> /dev/null; then
+#     sudo apt install apt-transport-https curl
+#     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+#     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+#     sudo apt update
+#     sudo apt install -y brave-browser
+# else
+#     echo "$COMMAND found"
+# fi
+# FILE=~/.local/share/applications/freecad_realthunder.desktop
+# if [ -f "$FILE" ]; then
+#     echo "$FILE exists."
+# else
+#     pip3 install 'lxml == 4.6.3'
+#     python3 ./python_scripts/download_latest_freecad_from_github.py
+#     sudo chmod a+x ${HOME}/apps/freecad_realthunder/FreeCad_RealThunder.AppImage
+#     cat >$FILE <<EOL
+# [Desktop Entry]
+# Name=FreeCad_RealThunder
+# Comment=FreeCad RealThunder version
+# Exec=${HOME}/apps/freecad_realthunder/FreeCad_RealThunder.AppImage
+# Icon=${HOME}/apps/app-icons/freecad_realthunder.png
+# Terminal=false
+# Type=Application
+# Categories=Development
+# EOL
+# fi
 COMMAND=google-earth-pro
 if ! command -v $COMMAND &> /dev/null; then
     wget -O ~/google-earth.deb https://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
@@ -237,21 +255,21 @@ if ! command -v $COMMAND &> /dev/null; then
 else
     echo "$COMMAND found"
 fi
-FILE=~/.local/share/applications/cura.desktop
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-else
-    curl https://software.ultimaker.com/cura/Ultimaker_Cura-4.13.1.AppImage --create-dirs -o ${HOME}/apps/cura/cura.AppImage
-    curl https://user-images.githubusercontent.com/18035735/48554277-46064580-e8de-11e8-8c4c-b682081a2219.png -o ${HOME}/apps/app-icons/cura.png
-    sudo chmod a+x ${HOME}/apps/cura/cura.AppImage
-    cat >$FILE <<EOL
-[Desktop Entry]
-Name=Cura
-Comment=Cura
-Exec=${HOME}/apps/cura/cura.AppImage
-Icon=${HOME}/apps/app-icons/cura.png
-Terminal=false
-Type=Application
-Categories=Development
-EOL
-fi
+# FILE=~/.local/share/applications/cura.desktop
+# if [ -f "$FILE" ]; then
+#     echo "$FILE exists."
+# else
+#     curl https://software.ultimaker.com/cura/Ultimaker_Cura-4.13.1.AppImage --create-dirs -o ${HOME}/apps/cura/cura.AppImage
+#     curl https://user-images.githubusercontent.com/18035735/48554277-46064580-e8de-11e8-8c4c-b682081a2219.png -o ${HOME}/apps/app-icons/cura.png
+#     sudo chmod a+x ${HOME}/apps/cura/cura.AppImage
+#     cat >$FILE <<EOL
+# [Desktop Entry]
+# Name=Cura
+# Comment=Cura
+# Exec=${HOME}/apps/cura/cura.AppImage
+# Icon=${HOME}/apps/app-icons/cura.png
+# Terminal=false
+# Type=Application
+# Categories=Development
+# EOL
+# fi
