c964e659a1697841e5eab8ed4c31224f18e86c64
Lucas Manchine <lucasperinm@gmail.com>
2023-01-17 16:08:52 -0300

    backup

--- new_system.sh
+++ new_system.sh
@@ -8,7 +8,7 @@ sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo
 # - Fritzing
 
 # lines below sudo apt install, install docker requirements
-sudo apt install -y gdebi python3-pip python3-venv htop libcanberra-gtk-module p7zip-full lm-sensors wireshark ppa-purge wireguard wireguard-tools net-tools nmap \
+sudo apt install -y gdebi python3-pip python3-venv htop libcanberra-gtk-module p7zip-full lm-sensors wireshark wireguard wireguard-tools net-tools nmap \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -17,7 +17,7 @@ sudo apt install -y gdebi python3-pip python3-venv htop libcanberra-gtk-module p
 
 ## system extra settings
 # allows gnome workspace to work with 2 monitors instead of only one
-gsettings set org.gnome.mutter workspaces-only-on-primary false
+# gsettings set org.gnome.mutter workspaces-only-on-primary false
 
 ## /#
 
@@ -27,65 +27,66 @@ gsettings set org.gnome.mutter workspaces-only-on-primary false
 # IF command doesn't exist run code block of installation
 # ELSE tell command exists 
 # FI
-COMMAND=google-chrome
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo gdebi -n ~/google-chrome.deb
-else
-    echo "$COMMAND found"
-fi
-COMMAND=code
-if ! command -v $COMMAND &> /dev/null; then
-    sudo apt-get install -y wget gpg
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-    rm -f packages.microsoft.gpg
-    sudo apt update
-    sudo apt install -y code
-    declare -a StringArray=(
-        'Dart-Code.dart-code'
-        'Dart-Code.flutter'
-        'DavidAnson.vscode-markdownlint'
-        'GitHub.copilot'
-        'GitHub.vscode-pull-request-github'
-        'GrapeCity.gc-excelviewer'
-        'ms-azuretools.vscode-docker'
-        'ms-python.python'
-        'ms-python.vscode-pylance'
-        'ms-toolsai.jupyter'
-        'ms-vscode-remote.remote-ssh'
-        'ms-vscode-remote.remote-ssh-edit'
-        'ms-vscode.cpptools'
-        'tomoki1207.pdf'
-        'WakaTime.vscode-wakatime'
-        'yzane.markdown-pdf'
-    )
-    for val in "${StringArray[@]}"; do
-        code --install-extension $val
-    done
-    pip install ipykernel
-else
-    echo "$COMMAND found"
-fi
+# COMMAND=google-chrome
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#     sudo gdebi -n ~/google-chrome.deb
+# else
+#     echo "$COMMAND found"
+# fi
+# COMMAND=code
+# if ! command -v $COMMAND &> /dev/null; then
+#     sudo apt-get install -y wget gpg
+#     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+#     sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+#     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+#     rm -f packages.microsoft.gpg
+#     sudo apt update
+#     sudo apt install -y code
+#     declare -a StringArray=(
+#         'Dart-Code.dart-code'
+#         'Dart-Code.flutter'
+#         'DavidAnson.vscode-markdownlint'
+#         'GitHub.copilot'
+#         'GitHub.vscode-pull-request-github'
+#         'GrapeCity.gc-excelviewer'
+#         'ms-azuretools.vscode-docker'
+#         'ms-python.python'
+#         'ms-python.vscode-pylance'
+#         'ms-toolsai.jupyter'
+#         'ms-vscode-remote.remote-ssh'
+#         'ms-vscode-remote.remote-ssh-edit'
+#         'ms-vscode.cpptools'
+#         'tomoki1207.pdf'
+#         'WakaTime.vscode-wakatime'
+#         'yzane.markdown-pdf'
+#     )
+#     for val in "${StringArray[@]}"; do
+#         code --install-extension $val
+#     done
+#     pip install ipykernel
+# else
+#     echo "$COMMAND found"
+# fi
 COMMAND=docker
 if ! command -v $COMMAND &> /dev/null; then
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 else
     echo "$COMMAND found"
 fi
-COMMAND=docker-compose
-if ! command -v $COMMAND &> /dev/null; then
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-else
-    echo "$COMMAND found"
-fi
+# COMMAND=docker-compose
+# if ! command -v $COMMAND &> /dev/null; then
+#     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#     sudo chmod +x /usr/local/bin/docker-compose
+# else
+#     echo "$COMMAND found"
+# fi
 # COMMAND=drovio
 # if ! command -v $COMMAND &> /dev/null; then
 #     wget -O ~/drovio.deb https://repository.drovio.com/stable/drovio/linux/latest_version/drovio.deb
@@ -93,47 +94,47 @@ fi
 # else
 #     echo "$COMMAND found"
 # fi
-COMMAND=spotify
-if ! command -v $COMMAND &> /dev/null; then
-    # curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo gpg --dearmor -o /usr/share/keyrings/spotify-archive-keyring.gpg
-    # echo "deb [signed-by=/usr/share/keyrings/spotify-archive-keyring.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-    # sudo apt-get update && sudo apt-get install spotify-client
-    sudo snap install spotify
-else
-    echo "$COMMAND found"
-fi
-COMMAND=obs
-if ! command -v $COMMAND &> /dev/null; then
-    # sudo apt install -y ffmpeg
-    # sudo add-apt-repository ppa:obsproject/obs-studio -y
-    # sudo apt update
-    sudo apt install -y obs-studio
-else
-    echo "$COMMAND found"
-fi
-COMMAND=discord
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-    sudo gdebi -n ~/discord.deb
-else
-    echo "$COMMAND found"
-fi
-# run telegram-desktop afterwards
-COMMAND=telegram-desktop
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O- https://telegram.org/dl/desktop/linux | sudo tar xJ -C /opt/
-    sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
-else
-    echo "$COMMAND found"
-fi
-COMMAND=slack
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/slack.deb https://downloads.slack-edge.com/releases/linux/4.25.0/prod/x64/slack-desktop-4.25.0-amd64.deb
-    sudo gdebi -n ~/slack.deb
-    # sudo snap install slack --classic
-else
-    echo "$COMMAND found"
-fi
+# COMMAND=spotify
+# if ! command -v $COMMAND &> /dev/null; then
+#     # curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo gpg --dearmor -o /usr/share/keyrings/spotify-archive-keyring.gpg
+#     # echo "deb [signed-by=/usr/share/keyrings/spotify-archive-keyring.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+#     # sudo apt-get update && sudo apt-get install spotify-client
+#     sudo snap install spotify
+# else
+#     echo "$COMMAND found"
+# fi
+# COMMAND=obs
+# if ! command -v $COMMAND &> /dev/null; then
+#     # sudo apt install -y ffmpeg
+#     # sudo add-apt-repository ppa:obsproject/obs-studio -y
+#     # sudo apt update
+#     sudo apt install -y obs-studio
+# else
+#     echo "$COMMAND found"
+# fi
+# COMMAND=discord
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+#     sudo gdebi -n ~/discord.deb
+# else
+#     echo "$COMMAND found"
+# fi
+# # run telegram-desktop afterwards
+# COMMAND=telegram-desktop
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O- https://telegram.org/dl/desktop/linux | sudo tar xJ -C /opt/
+#     sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
+# else
+#     echo "$COMMAND found"
+# fi
+# COMMAND=slack
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/slack.deb https://downloads.slack-edge.com/releases/linux/4.25.0/prod/x64/slack-desktop-4.25.0-amd64.deb
+#     sudo gdebi -n ~/slack.deb
+#     # sudo snap install slack --classic
+# else
+#     echo "$COMMAND found"
+# fi
 # COMMAND=upwork
 # if ! command -v $COMMAND &> /dev/null; then
 #     wget -O ~/upwork.deb https://upwork-usw2-desktopapp.upwork.com/binaries/v5_6_10_1_de501d28cc034306/upwork_5.6.10.1_amd64.deb
@@ -141,35 +142,35 @@ fi
 # else
 #     echo "$COMMAND found"
 # fi
-COMMAND=smplayer
-if ! command -v $COMMAND &> /dev/null; then
-    # sudo add-apt-repository ppa:rvm/smplayer -y
-    # sudo apt-get update 
-    # sudo apt-get install -y smplayer smplayer-themes smplayer-skins 
-    sudo snap install smplayer
-else
-    echo "$COMMAND found"
-fi
-COMMAND=youtube-dl
-if ! command -v $COMMAND &> /dev/null; then
-    sudo -H pip install --upgrade youtube-dl
-else
-    echo "$COMMAND found"
-fi
-COMMAND=obsidian
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v0.14.6/obsidian_0.14.6_amd64.deb
-    sudo gdebi -n ~/obsidian.deb
-else
-    echo "$COMMAND found"
-fi
-COMMAND=projectlibre
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/projectlibre.deb https://megalink.dl.sourceforge.net/project/projectlibre/ProjectLibre/1.9.3/projectlibre_1.9.3-1.deb
-    sudo gdebi -n ~/projectlibre.deb
-else
-    echo "$COMMAND found"
-fi
+# COMMAND=smplayer
+# if ! command -v $COMMAND &> /dev/null; then
+#     # sudo add-apt-repository ppa:rvm/smplayer -y
+#     # sudo apt-get update 
+#     # sudo apt-get install -y smplayer smplayer-themes smplayer-skins 
+#     sudo snap install smplayer
+# else
+#     echo "$COMMAND found"
+# fi
+# COMMAND=youtube-dl
+# if ! command -v $COMMAND &> /dev/null; then
+#     sudo -H pip install --upgrade youtube-dl
+# else
+#     echo "$COMMAND found"
+# fi
+# COMMAND=obsidian
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v0.14.6/obsidian_0.14.6_amd64.deb
+#     sudo gdebi -n ~/obsidian.deb
+# else
+#     echo "$COMMAND found"
+# fi
+# COMMAND=projectlibre
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/projectlibre.deb https://megalink.dl.sourceforge.net/project/projectlibre/ProjectLibre/1.9.3/projectlibre_1.9.3-1.deb
+#     sudo gdebi -n ~/projectlibre.deb
+# else
+#     echo "$COMMAND found"
+# fi
 # COMMAND=arduino
 # if ! command -v $COMMAND &> /dev/null; then
 #     wget -O ~/arduino1.tar.xz https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz
@@ -254,14 +255,14 @@ fi
 # Categories=Development
 # EOL
 # fi
-COMMAND=google-earth-pro
-if ! command -v $COMMAND &> /dev/null; then
-    wget -O ~/google-earth.deb https://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
-    sudo gdebi -n ~/google-earth.deb
-    rm ~/google-earth.deb
-else
-    echo "$COMMAND found"
-fi
+# COMMAND=google-earth-pro
+# if ! command -v $COMMAND &> /dev/null; then
+#     wget -O ~/google-earth.deb https://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
+#     sudo gdebi -n ~/google-earth.deb
+#     rm ~/google-earth.deb
+# else
+#     echo "$COMMAND found"
+# fi
 # FILE=~/.local/share/applications/cura.desktop
 # if [ -f "$FILE" ]; then
 #     echo "$FILE exists."
