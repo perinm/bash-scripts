1062ddf93abafffaa43d3dfa8dc55b4afd60f941
Lucas Manchine <lucasperinm@gmail.com>
2023-02-02 18:44:58 -0300

    backup wsl nvidia docker

--- nvidia_wsl/nvidia_notebook_wsl.sh
+++ nvidia_wsl/nvidia_notebook_wsl.sh
@@ -7,3 +7,11 @@ sudo gdebi -n cuda-repo-wsl-ubuntu-12-0-local_12.0.0-1_amd64.deb
 sudo cp /var/cuda-repo-wsl-ubuntu-12-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+#       && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+#       && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+#             sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+#             sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# sudo apt-get update
+# sudo apt-get install -y nvidia-docker2
