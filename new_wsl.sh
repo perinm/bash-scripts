88c71cbf037be38fd0b8aa85f3ff93bbe3c7c01f
Lucas Manchine <lucasperinm@gmail.com>
2023-02-11 00:02:34 -0300

    renamed files from new_system.sh to what system they use in place of new_'system'

--- new_wsl.sh
+++ new_wsl.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 
-sudo apt install -y gdebi python-is-python3 python3-pip python3-venv htop libcanberra-gtk-module p7zip-full lm-sensors wireshark \
-    ncdu ppa-purge wireguard wireguard-tools net-tools nmap gparted btrfs-progs copyq gnome-shell-extensions d-feet btrfs-compsize \
-    copyq gimp tilix minidlna whois \
-    apt-transport-https \
+sudo apt install -y gdebi python3-pip python3-venv htop p7zip-full lm-sensors \
+    ncdu ppa-purge nmap whois \
     ca-certificates \
     curl \
     gnupg \
@@ -13,14 +11,14 @@ sudo apt install -y gdebi python-is-python3 python3-pip python3-venv htop libcan
 
 COMMAND=docker
 if ! command -v $COMMAND &> /dev/null; then
-    sudo mkdir -p /etc/apt/keyrings
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-    sudo service docker start
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    # sudo service docker start
 else
     echo "$COMMAND found"
 fi
