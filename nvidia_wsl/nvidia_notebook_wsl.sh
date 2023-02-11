811c8da28ae85da78d2b143634d2de25d63285a1
Lucas Manchine <lucasperinm@gmail.com>
2023-02-10 23:49:52 -0300

    minor backup

--- nvidia_wsl/nvidia_notebook_wsl.sh
+++ nvidia_wsl/nvidia_notebook_wsl.sh
@@ -8,10 +8,10 @@ sudo cp /var/cuda-repo-wsl-ubuntu-12-0-local/cuda-*-keyring.gpg /usr/share/keyri
 sudo apt-get update
 sudo apt-get -y install cuda
 # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-#       && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-#       && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-#             sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-#             sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-# sudo apt-get update
-# sudo apt-get install -y nvidia-docker2
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
