5e3c73da62580d364ad3fa832524169db0b091ac
Lucas Manchine <lucasperinm@gmail.com>
2023-02-17 16:43:34 -0300

    backup ubuntu nvidia config

---
+++ nvidia_wsl/nvidia_ubuntu.sh
@@ -0,0 +1,11 @@
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+distribution=ubuntu22.04
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+sudo docker run --rm --runtime=nvidia --gpus all nvidia/cuda:11.6.2-base-ubuntu20.04 nvidia-smi
