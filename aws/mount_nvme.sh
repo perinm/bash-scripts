fa22ef04c9f15c50f89c37ad393b1ccdca5397b1
Lucas Manchine <lucasperinm@gmail.com>
2023-05-26 14:57:40 -0300

    updated aws

--- aws/mount_nvme.sh
+++ aws/mount_nvme.sh
@@ -2,4 +2,7 @@
 sudo mkfs -t xfs /dev/nvme1n1
 sudo mkdir -p /data
 sudo mount /dev/nvme1n1 /data
+sudo mkdir -p /data/docker
 sudo systemctl disable --now docker
+sudo usermod -aG docker $USER
+sudo chown -R $USER:$USER /data
