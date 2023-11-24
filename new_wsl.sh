23fb4cfdbc769f3e6aafefcd34f81182ed7e2a22
Lucas Manchine <lucasperinm@gmail.com>
2023-11-24 10:08:46 -0300

    removing nvidia part from new_wsl

--- new_wsl.sh
+++ new_wsl.sh
@@ -40,36 +40,36 @@ else
   echo "$COMMAND found"
 fi
 
-COMMAND=nvidia-smi
-if ! command -v $COMMAND &> /dev/null; then
-  echo "$COMMAND not found"
-else
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-  # curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-  # curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-  # sudo apt-get update
-  # sudo apt-get install -y nvidia-docker2
-  # wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
-  # sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-  # wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-wsl-ubuntu-12-1-local_12.1.0-1_amd64.deb
-  # sudo dpkg -i cuda-repo-wsl-ubuntu-12-1-local_12.1.0-1_amd64.deb
-  # sudo cp /var/cuda-repo-wsl-ubuntu-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
-  # sudo apt-get update
-  # sudo apt-get -y install cuda
+# COMMAND=nvidia-smi
+# if ! command -v $COMMAND &> /dev/null; then
+#   echo "$COMMAND not found"
+# else
+#   distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+#   # curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+#   # curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+#   # sudo apt-get update
+#   # sudo apt-get install -y nvidia-docker2
+#   # wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+#   # sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+#   # wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-wsl-ubuntu-12-1-local_12.1.0-1_amd64.deb
+#   # sudo dpkg -i cuda-repo-wsl-ubuntu-12-1-local_12.1.0-1_amd64.deb
+#   # sudo cp /var/cuda-repo-wsl-ubuntu-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+#   # sudo apt-get update
+#   # sudo apt-get -y install cuda
 
-  # https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=WSL-Ubuntu&target_version=2.0&target_type=deb_network
-  wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
-  sudo dpkg -i cuda-keyring_1.1-1_all.deb
-  sudo apt-get update
-  sudo apt-get -y install cuda
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-    && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-    && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-      sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-      sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-  sudo apt-get install -y nvidia-container-toolkit
-  sudo nvidia-ctk runtime configure --runtime=docker
-fi
+#   # https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=WSL-Ubuntu&target_version=2.0&target_type=deb_network
+#   wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+#   sudo dpkg -i cuda-keyring_1.1-1_all.deb
+#   sudo apt-get update
+#   sudo apt-get -y install cuda
+#   distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+#     && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+#     && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+#       sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+#       sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+#   sudo apt-get install -y nvidia-container-toolkit
+#   sudo nvidia-ctk runtime configure --runtime=docker
+# fi
 
 ubuntu_release=`lsb_release -rs`
 wget https://packages.microsoft.com/config/ubuntu/${ubuntu_release}/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
