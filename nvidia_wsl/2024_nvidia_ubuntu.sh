8a207f76b700dd4b96663acf87e1795f2e0aa07c
Lucas Manchine <lucasperinm@gmail.com>
2024-08-08 10:28:30 -0300

    nvidia wsl new script

---
+++ nvidia_wsl/2024_nvidia_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=WSL-Ubuntu&target_version=2.0&target_type=deb_network
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-5
