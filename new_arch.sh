3166686767534ce7880433361368c8bf62139329
Lucas Manchine <lucasperinm@gmail.com>
2024-08-28 16:44:12 -0300

    added pwgen

--- new_arch.sh
+++ new_arch.sh
@@ -11,7 +11,7 @@ sudo pacman -Syu --needed --noconfirm \
     gnome-system-monitor libvirt bridge-utils virt-manager jq firefox clutter \
     mpv ghex imagemagick ghostscript hwinfo bluez bluez-utils gnome-browser-connector \
     nano discord solaar less os-prober openvpn networkmanager-openvpn \
-    pipewire-alsa pavucontrol sof-firmware sof-tools tlp \
+    pipewire-alsa pavucontrol sof-firmware sof-tools tlp pwgen \
     gnome-shell-extension-appindicator
 
 sudo localectl set-locale LANG=en_US.UTF-8
