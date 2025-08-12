08973f0f3f6e0f47ae3481c6058e85cfa07b23e2
Lucas Manchine <lucasperinm@gmail.com>
2025-08-12 08:46:02 -0300

    adding default ideal git setting

--- new_mac.sh
+++ new_mac.sh
@@ -11,3 +11,6 @@ ZSHRC="${HOME}/.zshrc"
 if ! grep -qxF 'autoload -Uz compinit && compinit' "$ZSHRC" 2>/dev/null; then
   printf '\n%s\n' 'autoload -Uz compinit && compinit' >> "$ZSHRC"
 fi
+
+# Configure Git: auto-setup remote on first push
+git config --global push.autoSetupRemote true
